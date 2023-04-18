17c33eb2da94f6927c79e137d84b969e5b5255af
boneys <boneysekh@gmail.com>
2023-04-18 03:23:26 -0700

    Add second worker for batch processing API in the docs

--- src/main/java/io/orkes/example/banking/pojos/DepositDetail.java
+++ src/main/java/io/orkes/example/banking/pojos/DepositDetail.java
@@ -1,10 +1,12 @@
 package io.orkes.example.banking.pojos;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class DepositDetail {
 
     private String accountId;
--- src/main/java/io/orkes/example/banking/workers/ConductorWorkers.java
+++ src/main/java/io/orkes/example/banking/workers/ConductorWorkers.java
@@ -1,21 +1,33 @@
 package io.orkes.example.banking.workers;
 
+import com.netflix.conductor.sdk.workflow.task.InputParam;
 import com.netflix.conductor.sdk.workflow.task.WorkerTask;
 import io.orkes.example.banking.pojos.DepositDetail;
 import io.orkes.example.banking.pojos.FraudCheckResult;
 import io.orkes.example.banking.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @AllArgsConstructor
 @Component
+@Slf4j
 public class ConductorWorkers {
 
     private final FraudCheckService fraudCheckService;
+    private final Random random = new Random();
 
     // docs-marker-start-1
 
-    /** Note: Using this setting, up to 5 tasks will run in parallel, with tasks being polled every 200ms */
+    /**
+     * Note: Using this setting, up to 5 tasks will run in parallel, with tasks being polled every 200ms
+     */
     @WorkerTask(value = "fraud-check", threadCount = 5, pollingInterval = 200)
     public FraudCheckResult checkForFraudTask(DepositDetail depositDetail) {
         return fraudCheckService.checkForFraud(depositDetail);
@@ -23,6 +35,26 @@ public class ConductorWorkers {
 
     // docs-marker-end-1
 
+
+    // docs-marker-start-2
+
+    @WorkerTask(value = "retrieve-deposit-batch", threadCount = 5, pollingInterval = 200)
+    public List<DepositDetail> retrieveDepositBatch(@InputParam("batchCount") Integer batchCount) {
+        if (batchCount == null) {
+            batchCount = random.nextInt(5, 11);
+        }
+        batchCount = Math.min(100, batchCount); // Limit to 100 in playground
+        List<DepositDetail> depositDetails = IntStream.range(0, batchCount)
+                .mapToObj(i -> DepositDetail.builder()
+                        .accountId("acc-id-" + i)
+                        .amount(BigDecimal.valueOf(i * 1500L)) // Create random amounts
+                        .build())
+                .toList();
+        log.info("Returning {} transactions", depositDetails.size());
+        return depositDetails;
+    }
+
+    // docs-marker-end-2
 
 
 }
