ed4e2841c6f744e2b067aca195a8d3626bee3ad2
boneys <boneysekh@gmail.com>
2023-05-08 22:54:56 -0700

    Add constructor for Deposit Detail

--- src/main/java/io/orkes/example/banking/pojos/DepositDetail.java
+++ src/main/java/io/orkes/example/banking/pojos/DepositDetail.java
@@ -1,12 +1,16 @@
 package io.orkes.example.banking.pojos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepositDetail {
 
     private String accountId;
