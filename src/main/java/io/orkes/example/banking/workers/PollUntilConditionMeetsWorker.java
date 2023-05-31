1776f7878cfa65dd08f7f1a7a2a8e7cb312edd0c
boneys <boneysekh@gmail.com>
2023-05-30 20:14:35 -0700

    Add a new worker that supports polling until true

--- src/main/java/io/orkes/example/banking/workers/PollUntilConditionMeetsWorker.java
+++ src/main/java/io/orkes/example/banking/workers/PollUntilConditionMeetsWorker.java
@@ -5,11 +5,14 @@ import com.netflix.conductor.common.metadata.tasks.Task;
 import com.netflix.conductor.common.metadata.tasks.TaskResult;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class PollUntilConditionMeetsWorker implements Worker {
 
     public static final String CURRENT_ITERATION = "currentIteration";
     public static final String POLL_COUNTER = "pollCounter";
+    public static final String POLL_INTERVAL_SECONDS = "pollIntervalSeconds";
     int defaultPollCount = 3;
 
     @Override
@@ -21,25 +24,29 @@ public class PollUntilConditionMeetsWorker implements Worker {
     @Override
     public TaskResult execute(Task task) {
         TaskResult taskResult = new TaskResult(task);
-        if(!task.getInputData().containsKey(POLL_COUNTER)) {
+        if (!task.getInputData().containsKey(POLL_COUNTER)) {
             taskResult.addOutputData("message", "pollCounter param not found in input, will use default of " + defaultPollCount + " polls");
         }
-        int pollCounter = ((Number) task.getInputData().getOrDefault(POLL_COUNTER, defaultPollCount)).intValue();
-        if (pollCounter > 10) {
-            taskResult.addOutputData("message", "for this test worker, max poll count limit is 10");
-            pollCounter = 10;
-        }
-        taskResult.addOutputData(POLL_COUNTER, pollCounter);
 
-        // The task output data will be retained on the worker when polled
-        int currentIteration = ((Number) taskResult.getOutputData().getOrDefault(CURRENT_ITERATION, 0)).intValue();
-        taskResult.addOutputData(CURRENT_ITERATION, currentIteration);
+        int pollCounter = Math.min(10, castToInt(task.getInputData().getOrDefault(POLL_COUNTER, defaultPollCount)));
+        int pollIntervalSeconds = Math.min(10, castToInt(task.getInputData().getOrDefault(POLL_INTERVAL_SECONDS, 5)));
+
+        // Add these to the output for context
+        taskResult.addOutputData(POLL_INTERVAL_SECONDS, pollIntervalSeconds + " (this test task has a max limit of 10 seconds)");
+        taskResult.addOutputData(POLL_COUNTER, pollCounter + " (this test task has a max limit of 10 iterations)");
+
+        // We can read current iteration from the task output as the data will be retained on the worker when polled
+        int currentIteration = castToInt(taskResult.getOutputData().getOrDefault(CURRENT_ITERATION, 0));
+
+        // Increment the current iteration and set to the task output
+        taskResult.addOutputData(CURRENT_ITERATION, ++currentIteration);
+        taskResult.addOutputData("updatedTime", new Date().toString());
 
         // While condition is not met, keep task in progress
-        if(currentIteration < pollCounter) {
+        if (currentIteration < pollCounter) {
             taskResult.setStatus(TaskResult.Status.IN_PROGRESS);
-            // Set to 10 seconds to callback, but you can set this to any value as per the requirements
-            taskResult.setCallbackAfterSeconds(10);
+            // Set to configured seconds to callback, and you can set this to any value as per the requirements
+            taskResult.setCallbackAfterSeconds(pollIntervalSeconds);
             return taskResult;
         }
 
@@ -49,4 +56,13 @@ public class PollUntilConditionMeetsWorker implements Worker {
     }
     // docs-marker-end-1
 
+    public static Integer castToInt(Object obj) {
+        if (obj instanceof Number) {
+            return ((Number) obj).intValue();
+        }
+        return Integer.parseInt(obj.toString());
+    }
+
 }
+
+
