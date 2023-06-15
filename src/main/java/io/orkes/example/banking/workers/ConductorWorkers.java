16deb44f93bb3620ff6bb2234b4a4330149fd8d5
gardusig <gustavo.gardusi@gmail.com>
2023-06-15 18:27:32 -0300

    Removed unnecessary import

--- src/main/java/io/orkes/example/banking/workers/ConductorWorkers.java
+++ src/main/java/io/orkes/example/banking/workers/ConductorWorkers.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @AllArgsConstructor
